94ca37160365d5529639eac45b07380842c1dda4
Kgosi8 <127748278+Kgosi8@users.noreply.github.com>
2025-03-21 01:03:14 +0000

    frontend done

--- src/app/components/ai-vision/ai-vision.component.ts
+++ src/app/components/ai-vision/ai-vision.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, httpResource } from '@angular/common/http';
+import { ClaudeServiceService } from '../../services/claude-service.service';
+import { Observable } from 'rxjs';
+
 
 
 @Component({
@@ -12,7 +15,48 @@ export class AiVisionComponent {
 
   // URL for the image (this can be dynamically set)
   imageUrl: string = 'assets/brain.jpg';
-  constructor(){}
+  // response!:Observable<any>;
+  // constructor(private claudeService:ClaudeServiceService,private http:HttpClient){}
+  
 
-  getDescription(){}
+  getDescription()
+  {
+  //   const headers=new HttpHeaders({
+  //     'Content-Type':'applicatin/json',
+  //     'x-api-key':this.claudeService.apiKey,
+  //     'anthropic-version':'2023-06-01'
+  //   });
+  //   this.response=this.http.post(this.claudeService.baseUrl,JSON.stringify({
+  //     'model':'claude-3-opus-20240229',
+  //     'max-token':50,
+  //     'messages':{
+  //       'role':'user',
+  //       'content':[
+  //         {
+  //           'type':'image',
+  //           'source':{
+  //             'type':'base64',
+  //             'media-type':'image/jpeg',
+  //             'data':this.claudeService.base64Image,
+  //           }
+  //         },{
+  //           'type':'text',
+  //           'text':'please describe what you see in this image',
+  //         }
+          
+
+  //       ]
+  //     }
+  //   }),
+  //   {
+      
+  //   }
+  // )
+  // console.log(this.response);
+
+  // return this.response;
+  }
 }
+      
+      
+  
--- src/app/services/claude-service.service.ts
+++ src/app/services/claude-service.service.ts
@@ -4,7 +4,30 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ClaudeServiceService {
-  
+  baseUrl: string="http://api.anthropic.com/v1/messages";
+  apiKey: string='Your api-key';
+ 
+  base64Image: string | undefined;  // To store the base64 string of the image
 
-  constructor() { }
+  // This method is called when the user selects an image
+  changeToBase64(): void {
+    const file = "assets/brain.jpg";
+    
+    if (file) {
+      // Use FileReader to convert the image to Base64
+      const reader = new FileReader();
+      
+      // This will be triggered once the file is read
+      reader.onload = (e: any) => {
+        this.base64Image = e.target.result;  // Base64 string
+        console.log('Base64 Image:', this.base64Image);
+      };
+
+      // Read the image as a data URL (Base64 string)
+    }
+  }
+
+  constructor() { 
+    this.changeToBase64();
+  }
 }
